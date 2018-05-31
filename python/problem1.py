6dd83293fc64301bef4ac3e461dd2ea4db3c32d5
Michael Bianchi <michaeldbianchi@gmail.com>
2018-05-31 11:28:07 -0500

    Add one-liner as alternative solution

--- python/problem1.py
+++ python/problem1.py
@@ -17,6 +17,8 @@ def solve():
             sum += i
     return sum
 
+# neat one-liner
+# print(sum([i for i in range(1, 1000) if (i % 3 == 0 or i % 5 == 0)]))
 
 if __name__ == "__main__":
     print(solve())
